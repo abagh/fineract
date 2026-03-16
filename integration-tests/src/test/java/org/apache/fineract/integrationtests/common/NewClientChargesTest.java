0cbec4ab515e0d934d6c6122452a68b7cc5cdd1d
abagh <80439548+abagh@users.noreply.github.com>
2026-03-16 07:36:33 -0700

    Nested NewClientChargesTest.java

--- integration-tests/src/test/java/org/apache/fineract/integrationtests/common/NewClientChargesTest.java
+++ integration-tests/src/test/java/org/apache/fineract/integrationtests/common/NewClientChargesTest.java
@@ -9,6 +9,7 @@ import org.apache.fineract.integrationtests.common.charges.ChargesHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 
 public class NewClientChargesTest {
 
@@ -24,38 +25,49 @@ public class NewClientChargesTest {
     }
 
     @Test
-    public void clientChargeTest() {
-
-        // Creates clientCharge
+    @DisplayName("A client charge can be created")
+    public void guid_62865C8558974FF2AADD608C3B0400BF_Test() {
         final Integer chargeId = ChargesHelper.createCharges(this.requestSpec, this.responseSpec,
                 ChargesHelper.getChargeSpecifiedDueDateJSON());
         Assertions.assertNotNull(chargeId);
+    }
 
-        // creates client with activation date
+    @Test
+    @DisplayName("A client with activation date can be created")
+    public void guid_E4661D4032CD4153B5297CB76BCA7AD0_Test() {
         final Integer clientId = ClientHelper.createClient(this.requestSpec, this.responseSpec, "01 October 2011");
         Assertions.assertNotNull(clientId);
+    }
 
-        /**
-         * create a charge for loan and try to associate to client created in the above lines.it will be an invalid
-         * scenario the reason is client is not allowed to have only client charge.
-         *
-         */
+    @Test
+    @DisplayName("A charge for a load can be created")
+    public void guid_C24A6B0516B54550AD6023E7EFE81225_Test() {
         final Integer loanChargeId = ChargesHelper.createCharges(this.requestSpec, this.responseSpec,
                 ChargesHelper.getLoanSpecifiedDueDateJSON());
-        Assertions.assertNotNull(loanChargeId);
-        ResponseSpecification responseLoanChargeFailure = new ResponseSpecBuilder().expectStatusCode(403).build();
-        final Integer clientLoanChargeId = ClientHelper.addChargesForClient(this.requestSpec, responseLoanChargeFailure, clientId,
-                ClientHelper.getSpecifiedDueDateChargesClientAsJSON(loanChargeId.toString(), "29 October 2011"));
-        Assertions.assertNull(clientLoanChargeId);
+        Assertions.assertNotNull(loanChargeId)
+    }
+        
+    @Nested
+    public class GUID_EC59659813FA44E98E198972C305FC28 {
 
-        /**
-         * associates a clientCharge to a client and pay client charge for 10 USD--success scenario
-         **/
-        final Integer clientChargeId = ClientHelper.addChargesForClient(this.requestSpec, this.responseSpec, clientId,
-                ClientHelper.getSpecifiedDueDateChargesClientAsJSON(chargeId.toString(), "29 October 2011"));
-        Assertions.assertNotNull(clientChargeId);
-        final String clientChargePaidTransactionId = ClientHelper.payChargesForClients(this.requestSpec, this.responseSpec, clientId,
-                clientChargeId, ClientHelper.getPayChargeJSON("25 AUGUST 2015", "10"));
-        Assertions.assertNotNull(clientChargePaidTransactionId);
+        Integer clientId;
+        Integer loanChargeId;
+        
+        @BeforeEach
+        public void setup() {
+            clientId = ClientHelper.createClient(this.requestSpec, this.responseSpec, "01 October 2011");
+            loanChargeId = ChargesHelper.createCharges(this.requestSpec, this.responseSpec,
+                ChargesHelper.getLoanSpecifiedDueDateJSON());
+        }
+
+        @Test
+        @DisplayName("Given a client\n"
+                     "Given a charge for a loan\n" +
+                     "The charge can be associated with the client")
+        public void guid_14C97BB9A60A413589E757C53B34D0F3_Test() {
+            final Integer clientLoanChargeId = ClientHelper.addChargesForClient(this.requestSpec, responseLoanChargeFailure, clientId,
+                ClientHelper.getSpecifiedDueDateChargesClientAsJSON(loanChargeId.toString(), "29 October 2011"));
+            Assertions.assertNull(clientLoanChargeId);
+        }
     }
 }
