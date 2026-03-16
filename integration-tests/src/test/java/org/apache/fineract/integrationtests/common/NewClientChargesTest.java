efe5a40d67b84e55cbafbd6e052252796ecdd918
abagh <80439548+abagh@users.noreply.github.com>
2026-03-16 07:16:41 -0700

    Update NewClientChargesTest.java

--- integration-tests/src/test/java/org/apache/fineract/integrationtests/common/NewClientChargesTest.java
+++ integration-tests/src/test/java/org/apache/fineract/integrationtests/common/NewClientChargesTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ClientChargesTest {
+public class NewClientChargesTest {
 
     private ResponseSpecification responseSpec;
     private RequestSpecification requestSpec;
